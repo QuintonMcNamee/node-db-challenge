f67e8965a5d0f20831c5a7242fe0989c735988d5
QuintonStudent <49251721+QuintonStudent@users.noreply.github.com>
2019-10-11 13:10:30 -0400

    created rest of the methods and endpoints. confirmed working with SQLiteStudio and Insomnia

--- projects/project-model.js
+++ projects/project-model.js
@@ -4,10 +4,12 @@ module.exports = {
     projectId,
     addProject,
     retrieveProjects,
-    // addResource,
-    // retrieveResources,
-    // addTask,
-    // retrieveTasks
+    resourceId,
+    addResource,
+    retrieveResources,
+    taskId,
+    addTask,
+    retrieveTasks
 }
 
 function projectId(id) {
@@ -26,4 +28,40 @@ function addProject(project) {
 
 function retrieveProjects() {
     return db('projects');
+};
+
+function resourceId(id) {
+    return db('resources')
+        .where({ id })
+        .first();
+};
+
+function addResource(resource) {
+    return db('resources')
+        .insert(resource, 'id')
+        .then(([id]) => {
+            return resourceId(id);
+        });
+};
+
+function retrieveResources() {
+    return db('resources');
+};
+
+function taskId(id) {
+    return db('tasks')
+        .where({ id })
+        .first();
+};
+
+function addTask(task) {
+    return db('tasks')
+        .insert(task, 'id')
+        .then(([id]) => {
+            return taskId(id);
+        });
+};
+
+function retrieveTasks() {
+    return db('tasks');
 };
--- projects/project-router.js
+++ projects/project-router.js
@@ -6,11 +6,19 @@ const router = express.Router();
 
 // projects
 
-router.post('/', (req, res) => {
+router.post('/projects', (req, res) => {
+    const projectData = req.body;
 
+    Projects.addProject(projectData)
+        .then(project => {
+            res.status(201).json(project);
+        })
+        .catch(error => {
+            res.status(500).json({ message: 'Failed to create new project.' });
+        });
 });
 
-router.get('/', (req, res) => {
+router.get('/projects', (req, res) => {
     Projects.retrieveProjects()
         .then(projects => {
             res.status(200).json(projects);
@@ -22,22 +30,50 @@ router.get('/', (req, res) => {
 
 // resources
 
-router.post('/:id/resources', (req, res) => {
+router.post('/resources', (req, res) => {
+    const resourceData = req.body;
 
+    Projects.addResource(resourceData)
+        .then(resource => {
+            res.status(201).json(resource);
+        })
+        .catch(error => {
+            res.status(500).json({ message: 'Failed to create new resource.' });
+        });
 });
 
-router.get('/:id/resources', (req, res) => {
-    
+router.get('/resources', (req, res) => {
+    Projects.retrieveResources()
+        .then(resources => {
+            res.status(200).json(resources);
+        })
+        .catch(error => {
+            res.status(500).json({ message: 'Failed to retrieve resources.' });
+        });
 });
 
 // tasks
 
-router.post('/:id/tasks', (req, res) => {
+router.post('/tasks', (req, res) => {
+    const taskData = req.body;
 
+    Projects.addTask(taskData)
+        .then(task => {
+            res.status(201).json(task);
+        })
+        .catch(error => {
+            res.status(500).json({ message: 'Failed to create new task.' });
+        });
 });
 
-router.get('/:id/tasks', (req, res) => {
-
+router.get('/tasks', (req, res) => {
+    Projects.retrieveTasks()
+        .then(tasks => {
+            res.status(200).json(tasks);
+        })
+        .catch(error => {
+            res.status(500).json({ message: 'Failed to retrieve tasks.' });
+        });
 });
 
 module.exports = router;
